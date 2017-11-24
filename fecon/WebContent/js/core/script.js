6d434dae39f8d63a3bf38d2c8f4cca2600430d60
LDR Software <LDR Software@192.168.0.164>
2017-11-24 01:27:39 +0100

    v.0.00.30

--- fecon/WebContent/js/core/script.js
+++ fecon/WebContent/js/core/script.js
@@ -10,10 +10,10 @@
 //Configuracion
 var app = angular.module('app', ['ngRoute','ngMaterial']);
 //URL Desarrollo
-//var targetHost = 'http://localhost:8080/fecon/';
+var targetHost = 'http://localhost:8080/fecon/';
 //URL Produccion (tengo que conseguir que funcione ./ y entonces valdría para prod y desa, pero para ello necesito que
 // en la URL de openshift no aparezca la aplicación
-var targetHost = 'http://fecon-luisda1983.193b.starter-ca-central-1.openshiftapps.com/';
+//var targetHost = 'http://fecon-luisda1983.193b.starter-ca-central-1.openshiftapps.com/';
 //var targetHost = 'http://fecon-luisda1983.rhcloud.com/';
 //var targetHost = 'http://fecon-luisda1983.1d35.starter-us-east-1.openshiftapps.com/fecon';
 //Configuracion de tema primario y secundario
