401da82a1431647c5605aa05f34e12a28eca37f0
LDR Software <LDR Software@192.168.0.164>
2017-12-20 23:17:19 +0100

    (INST) V.0.00.41 - Gestión manual de apuntes respecto al presupuesto

--- fecon/WebContent/js/core/route.js
+++ fecon/WebContent/js/core/route.js
@@ -3,71 +3,71 @@ app.config(function($routeProvider) {
 	
 	$routeProvider
 		.when('/', {
-			templateUrl : 'pages/home.html?v.0.00.40',
+			templateUrl : 'pages/home.html?v.0.00.41',
 			controller  : 'homeController'
 		})
 		.when('/lgon', {
-			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.40',
+			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.41',
 			controller  : 'usuaLgonCtrl'
 		})
 		.when('/logout', {
-			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.40',
+			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.41',
 			controller  : 'usuaExitCtrl'
 		})
 		.when('/usua/regi', {
-			templateUrl : 'pages/core/usua/usuaRegi.html?v.0.00.40',
+			templateUrl : 'pages/core/usua/usuaRegi.html?v.0.00.41',
 			controller  : 'usuaRegiCtrl'
 		})
 		.when('/avis/list', {
-			templateUrl : 'pages/core/avis/avisList.html?v.0.00.40',
+			templateUrl : 'pages/core/avis/avisList.html?v.0.00.41',
 			controller  : 'avisListCtrl'
 		})
 		.when('/invi/list', {
-			templateUrl : 'pages/core/invi/inviList.html?v.0.00.40',
+			templateUrl : 'pages/core/invi/inviList.html?v.0.00.41',
 			controller  : 'inviListCtrl'
 		})
 		.when('/cuen/list', {
-			templateUrl : 'pages/appl/cuen/cuenList.html?v.0.00.40',
+			templateUrl : 'pages/appl/cuen/cuenList.html?v.0.00.41',
 			controller  : 'cuenListCtrl'
 		})
 		.when('/cuen/cuad', {
-			templateUrl : 'pages/appl/cuen/cuenCuad.html?v.0.00.40',
+			templateUrl : 'pages/appl/cuen/cuenCuad.html?v.0.00.41',
 			controller  : 'cuenCuadCtrl'
 		})
 		.when('/cuen/tras/', {
-			templateUrl : 'pages/appl/cuen/cuenTras.html?v.0.00.40',
+			templateUrl : 'pages/appl/cuen/cuenTras.html?v.0.00.41',
 			controller  : 'cuenTrasCtrl'
 		})
 		.when('/pres/resu', {
-			templateUrl : 'pages/appl/pres/presResu.html?v.0.00.40',
+			templateUrl : 'pages/appl/pres/presResu.html?v.0.00.41',
 			controller  : 'presResuCtrl'
 		})
 		.when('/pres/anua', {
-			templateUrl : 'pages/appl/pres/presAnua.html?v.0.00.40',
+			templateUrl : 'pages/appl/pres/presAnua.html?v.0.00.41',
 			controller  : 'presAnuaCtrl'
 		})
 		.when('/pres/mesp', {
-			templateUrl : 'pages/appl/pres/presMesp.html?v.0.00.40',
+			templateUrl : 'pages/appl/pres/presMesp.html?v.0.00.41',
 			controller  : 'presMespCtrl'
 		})
 		.when('/pres/conc', {
-			templateUrl : 'pages/appl/pres/presConc.html?v.0.00.40',
+			templateUrl : 'pages/appl/pres/presConc.html?v.0.00.41',
 			controller  : 'presConcCtrl'
 		})
 		.when('/hcon/apun', {
-			templateUrl : 'pages/appl/hcon/hconApun.html?v.0.00.40',
+			templateUrl : 'pages/appl/hcon/hconApun.html?v.0.00.41',
 			controller  : 'hconApunCtrl'
 		})
 		.when('/hcon/list/', {
-			templateUrl : 'pages/appl/hcon/hconList.html?v.0.00.40',
+			templateUrl : 'pages/appl/hcon/hconList.html?v.0.00.41',
 			controller  : 'hconListCtrl'
 		})
 		.when('/pres/nuev', {
-			templateUrl : 'pages/pres/presNuev.html?v.0.00.40',
+			templateUrl : 'pages/pres/presNuev.html?v.0.00.41',
 			controller  : 'presNuevController'
 		})
 		.when('/adm/conc', {
-			templateUrl : 'pages/adm/conc.html?v.0.00.40',
+			templateUrl : 'pages/adm/conc.html?v.0.00.41',
 			controller  : 'admConcCtrl'
 		})
 		.otherwise({
