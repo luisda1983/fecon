7332340a97825ca7d3c2a7ac3f8018d25381c3b6
LDR Software <LDR Software@192.168.1.196>
2017-12-31 23:08:26 +0100

    Inc.0.00.42-1 - Aplicación no funciona
    Se ha subido el script.js incorrecto apuntando a localhost.

--- fecon/WebContent/js/core/route.js
+++ fecon/WebContent/js/core/route.js
@@ -3,71 +3,79 @@ app.config(function($routeProvider) {
 	
 	$routeProvider
 		.when('/', {
-			templateUrl : 'pages/home.html?v.0.00.41',
+			templateUrl : 'pages/home.html?v.0.00.42',
 			controller  : 'homeController'
 		})
 		.when('/lgon', {
-			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.41',
+			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.42',
 			controller  : 'usuaLgonCtrl'
 		})
 		.when('/logout', {
-			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.41',
+			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.42',
 			controller  : 'usuaExitCtrl'
 		})
 		.when('/usua/regi', {
-			templateUrl : 'pages/core/usua/usuaRegi.html?v.0.00.41',
+			templateUrl : 'pages/core/usua/usuaRegi.html?v.0.00.42',
 			controller  : 'usuaRegiCtrl'
 		})
 		.when('/avis/list', {
-			templateUrl : 'pages/core/avis/avisList.html?v.0.00.41',
+			templateUrl : 'pages/core/avis/avisList.html?v.0.00.42',
 			controller  : 'avisListCtrl'
 		})
 		.when('/invi/list', {
-			templateUrl : 'pages/core/invi/inviList.html?v.0.00.41',
+			templateUrl : 'pages/core/invi/inviList.html?v.0.00.42',
 			controller  : 'inviListCtrl'
 		})
 		.when('/cuen/list', {
-			templateUrl : 'pages/appl/cuen/cuenList.html?v.0.00.41',
+			templateUrl : 'pages/appl/cuen/cuenList.html?v.0.00.42',
 			controller  : 'cuenListCtrl'
 		})
 		.when('/cuen/cuad', {
-			templateUrl : 'pages/appl/cuen/cuenCuad.html?v.0.00.41',
+			templateUrl : 'pages/appl/cuen/cuenCuad.html?v.0.00.42',
 			controller  : 'cuenCuadCtrl'
 		})
 		.when('/cuen/tras/', {
-			templateUrl : 'pages/appl/cuen/cuenTras.html?v.0.00.41',
+			templateUrl : 'pages/appl/cuen/cuenTras.html?v.0.00.42',
 			controller  : 'cuenTrasCtrl'
 		})
 		.when('/pres/resu', {
-			templateUrl : 'pages/appl/pres/presResu.html?v.0.00.41',
+			templateUrl : 'pages/appl/pres/presResu.html?v.0.00.42',
 			controller  : 'presResuCtrl'
 		})
 		.when('/pres/anua', {
-			templateUrl : 'pages/appl/pres/presAnua.html?v.0.00.41',
+			templateUrl : 'pages/appl/pres/presAnua.html?v.0.00.42',
 			controller  : 'presAnuaCtrl'
 		})
 		.when('/pres/mesp', {
-			templateUrl : 'pages/appl/pres/presMesp.html?v.0.00.41',
+			templateUrl : 'pages/appl/pres/presMesp.html?v.0.00.42',
 			controller  : 'presMespCtrl'
 		})
 		.when('/pres/conc', {
-			templateUrl : 'pages/appl/pres/presConc.html?v.0.00.41',
+			templateUrl : 'pages/appl/pres/presConc.html?v.0.00.42',
 			controller  : 'presConcCtrl'
 		})
 		.when('/hcon/apun', {
-			templateUrl : 'pages/appl/hcon/hconApun.html?v.0.00.41',
+			templateUrl : 'pages/appl/hcon/hconApun.html?v.0.00.42',
 			controller  : 'hconApunCtrl'
 		})
 		.when('/hcon/list/', {
-			templateUrl : 'pages/appl/hcon/hconList.html?v.0.00.41',
+			templateUrl : 'pages/appl/hcon/hconList.html?v.0.00.42',
 			controller  : 'hconListCtrl'
 		})
+		.when('/cate/list/', {
+			templateUrl : 'pages/appl/cate/cateList.html?v.0.00.42',
+			controller  : 'cateListCtrl'
+		})
+		.when('/cate/form', {
+			templateUrl : 'pages/appl/cate/cateForm.html?v.0.00.42',
+			controller  : 'cateFormCtrl'
+		})
 		.when('/pres/nuev', {
-			templateUrl : 'pages/pres/presNuev.html?v.0.00.41',
+			templateUrl : 'pages/pres/presNuev.html?v.0.00.42',
 			controller  : 'presNuevController'
 		})
 		.when('/adm/conc', {
-			templateUrl : 'pages/adm/conc.html?v.0.00.41',
+			templateUrl : 'pages/adm/conc.html?v.0.00.42',
 			controller  : 'admConcCtrl'
 		})
 		.otherwise({
