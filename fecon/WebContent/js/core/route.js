08bf14984db38d83b0d9d900bc0383463a0c55e1
LDR Software <LDR Software@192.168.0.164>
2018-02-08 23:32:58 +0100

    (INST) v.0.00.45 - Comm
    Centralización de todas las comunicaciones FrontEnd-BackEnd
    Normalización de contextos.
    Normalización de obtención de literales y parámetros de aplicación.

--- fecon/WebContent/js/core/route.js
+++ fecon/WebContent/js/core/route.js
@@ -3,83 +3,83 @@ app.config(function($routeProvider) {
 	
 	$routeProvider
 		.when('/', {
-			templateUrl : 'pages/home.html?v.0.00.43',
+			templateUrl : 'pages/home.html?v.0.00.45',
 			controller  : 'homeController'
 		})
 		.when('/lgon', {
-			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.43',
+			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.45',
 			controller  : 'usuaLgonCtrl'
 		})
 		.when('/logout', {
-			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.43',
+			templateUrl : 'pages/core/usua/usuaLgon.html?v.0.00.45',
 			controller  : 'usuaExitCtrl'
 		})
 		.when('/usua/regi', {
-			templateUrl : 'pages/core/usua/usuaRegi.html?v.0.00.43',
+			templateUrl : 'pages/core/usua/usuaRegi.html?v.0.00.45',
 			controller  : 'usuaRegiCtrl'
 		})
 		.when('/avis/list', {
-			templateUrl : 'pages/core/avis/avisList.html?v.0.00.43',
+			templateUrl : 'pages/core/avis/avisList.html?v.0.00.45',
 			controller  : 'avisListCtrl'
 		})
 		.when('/invi/list', {
-			templateUrl : 'pages/core/invi/inviList.html?v.0.00.43',
+			templateUrl : 'pages/core/invi/inviList.html?v.0.00.45',
 			controller  : 'inviListCtrl'
 		})
 		.when('/cuen/list', {
-			templateUrl : 'pages/appl/cuen/cuenList.html?v.0.00.43e',
+			templateUrl : 'pages/appl/cuen/cuenList.html?v.0.00.45',
 			controller  : 'cuenListCtrl'
 		})
 		.when('/cuen/form', {
-			templateUrl : 'pages/appl/cuen/cuenForm.html?v.0.00.43d',
+			templateUrl : 'pages/appl/cuen/cuenForm.html?v.0.00.45',
 			controller  : 'cuenFormCtrl'
 		})
 		.when('/cuen/cuad', {
-			templateUrl : 'pages/appl/cuen/cuenCuad.html?v.0.00.43c',
+			templateUrl : 'pages/appl/cuen/cuenCuad.html?v.0.00.45',
 			controller  : 'cuenCuadCtrl'
 		})
 		.when('/cuen/tras/', {
-			templateUrl : 'pages/appl/cuen/cuenTras.html?v.0.00.43d',
+			templateUrl : 'pages/appl/cuen/cuenTras.html?v.0.00.45',
 			controller  : 'cuenTrasCtrl'
 		})
 		.when('/pres/resu', {
-			templateUrl : 'pages/appl/pres/presResu.html?v.0.00.43',
+			templateUrl : 'pages/appl/pres/presResu.html?v.0.00.45',
 			controller  : 'presResuCtrl'
 		})
 		.when('/pres/anua', {
-			templateUrl : 'pages/appl/pres/presAnua.html?v.0.00.43',
+			templateUrl : 'pages/appl/pres/presAnua.html?v.0.00.45',
 			controller  : 'presAnuaCtrl'
 		})
 		.when('/pres/mesp', {
-			templateUrl : 'pages/appl/pres/presMesp.html?v.0.00.43',
+			templateUrl : 'pages/appl/pres/presMesp.html?v.0.00.45',
 			controller  : 'presMespCtrl'
 		})
 		.when('/pres/conc', {
-			templateUrl : 'pages/appl/pres/presConc.html?v.0.00.43',
+			templateUrl : 'pages/appl/pres/presConc.html?v.0.00.45',
 			controller  : 'presConcCtrl'
 		})
 		.when('/hcon/apun', {
-			templateUrl : 'pages/appl/hcon/hconForm.html?v.0.00.43',
+			templateUrl : 'pages/appl/hcon/hconForm.html?v.0.00.45',
 			controller  : 'hconFormCtrl'
 		})
 		.when('/hcon/list/', {
-			templateUrl : 'pages/appl/hcon/hconList.html?v.0.00.43',
+			templateUrl : 'pages/appl/hcon/hconList.html?v.0.00.45',
 			controller  : 'hconListCtrl'
 		})
 		.when('/cate/list/', {
-			templateUrl : 'pages/appl/cate/cateList.html?v.0.00.43d',
+			templateUrl : 'pages/appl/cate/cateList.html?v.0.00.45',
 			controller  : 'cateListCtrl'
 		})
 		.when('/cate/form', {
-			templateUrl : 'pages/appl/cate/cateForm.html?v.0.00.43d',
+			templateUrl : 'pages/appl/cate/cateForm.html?v.0.00.45',
 			controller  : 'cateFormCtrl'
 		})
 		.when('/conc/list', {
-			templateUrl : 'pages/appl/conc/concList.html?v.0.00.43d',
+			templateUrl : 'pages/appl/conc/concList.html?v.0.00.45',
 			controller  : 'concListCtrl'
 		})
 		.when('/conc/form', {
-			templateUrl : 'pages/appl/conc/concForm.html?v.0.00.43d',
+			templateUrl : 'pages/appl/conc/concForm.html?v.0.00.45',
 			controller  : 'concFormCtrl'
 		})
 		.otherwise({
