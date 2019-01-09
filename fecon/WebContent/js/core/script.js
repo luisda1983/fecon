bd93f90278a5e4efc23a2642e815ca3f4c85a5c3
LDR Software <LDR Software@LAPTOP-FPVDSJB2>
2019-01-09 23:12:27 +0100

    (INST) v.0.00.63 - Normalización Front-End
    Normalización del SW Front-End a la arquitectura definida.
    US-Server

--- fecon/WebContent/js/core/script.js
+++ fecon/WebContent/js/core/script.js
@@ -1,11 +1,10 @@
-///////////////////////////////////////////////////////////////////////////////////////////////
-// Configuracion principal de la aplicacion.                                                 //
-//    - Configuracion de desarrollo y produccion.                                            //
-//    - Configuracion de temas.                                                              //
-///////////////////////////////////////////////////////////////////////////////////////////////
-// Version   | F. Fin Desa | F. Install | Comentarios de version                             //
-// v.0.00.00 |  31/01/2017 | xx/xx/xxxx | Primera version del SW                             //
-///////////////////////////////////////////////////////////////////////////////////////////////
+//*****************************************************************************************************************//
+// Configuracion principal de la aplicacion.                                                                       //
+//    - Configuracion de desarrollo y produccion.                                                                  //
+//    - Configuracion de temas.                                                                                    //
+//*****************************************************************************************************************//
+// v.01.00.00 || 03.01.2019 || Versión Inicial                                                                     //
+//*****************************************************************************************************************//
 
 //Configuracion
 var app = angular.module('app', ['ngRoute','ngMaterial']);
@@ -13,8 +12,8 @@ var app = angular.module('app', ['ngRoute','ngMaterial']);
 //var targetHost = 'http://localhost:8080/fecon/';
 //URL Produccion (tengo que conseguir que funcione ./ y entonces valdría para prod y desa, pero para ello necesito que
 // en la URL de openshift no aparezca la aplicación
-var targetHost = 'http://fecon-luisda1983.193b.starter-ca-central-1.openshiftapps.com/';
-//var targetHost = 'http://fecon-luisda1983.1d35.starter-us-east-1.openshiftapps.com/';
+//var targetHost = 'http://fecon-luisda1983.193b.starter-ca-central-1.openshiftapps.com/';
+var targetHost = 'http://fecon-luisda1983.1d35.starter-us-east-1.openshiftapps.com/';
 //Configuracion de tema primario y secundario
 app.config(function($mdThemingProvider) {
 	$mdThemingProvider
