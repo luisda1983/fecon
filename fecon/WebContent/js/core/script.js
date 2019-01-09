032e241f19d2beae4a94547e1c8f1ffbc887d67b
LDR Software <LDR Software@LAPTOP-FPVDSJB2>
2019-01-09 23:21:44 +0100

    Contrucción del proyecto en US-Server

--- fecon/WebContent/js/core/script.js
+++ fecon/WebContent/js/core/script.js
@@ -14,6 +14,7 @@ var app = angular.module('app', ['ngRoute','ngMaterial']);
 // en la URL de openshift no aparezca la aplicación
 //var targetHost = 'http://fecon-luisda1983.193b.starter-ca-central-1.openshiftapps.com/';
 var targetHost = 'http://fecon-luisda1983.1d35.starter-us-east-1.openshiftapps.com/';
+
 //Configuracion de tema primario y secundario
 app.config(function($mdThemingProvider) {
 	$mdThemingProvider
